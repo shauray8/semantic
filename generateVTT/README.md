93443854159f338a29203411cd48cd284098db5e
shauray8 <shauray.209309012@muj.manipal.edu>
2023-05-10 09:48:17 +0530

    simple script which does not work

---
+++ generateVTT/README.md
@@ -0,0 +1,2 @@
+### Generate .vtt files for corresponding .wav files 
+I could not find subtitiles for all the videos I want to use so the next best thing I could think of is using Whisper and generate my own .vtt files (let's see how this thing turns out !)
--- raw_data_downloader.py
+++ raw_data_downloader.py
@@ -1,1 +1,16 @@
-## working on it 
+from pytube import YouTube
+
+# Ask the user for the YouTube video URL
+url = input("Enter the YouTube video URL: ")
+
+# Create a YouTube object from the URL
+yt = YouTube(url)
+
+# Get the highest resolution video stream
+stream = yt.streams.get_lowest_resolution()
+
+# Download the video
+print("Downloading...")
+stream.download()
+print("Download complete!")
+
